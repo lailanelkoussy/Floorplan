694d11ac19cfe418f150b95dc85163661c809949
Ali El-Said <palindali@gmail.com>
2020-04-10 07:08:05 +0200

    Implemented Cluster Growth Function. Created Floorplan and Coordinate classes. Modified Block class.

--- Block.py
+++ Block.py
@@ -1,11 +1,35 @@
 class Block:
 
-    def __init__(self, block_id, width, height, ):
-        self.width = width
-        self.height = height
-        self.block_id = block_id
-        self.connections = []  # assuming connections are pair relations
-        self.placed = False
+	def __init__(self, block_id, width, height, ):
+		self.width = width
+		self.height = height
+		self.block_id = block_id
+		self.connections = []  # assuming connections are pair relations
+		self.x = -1
+		self.y = -1
+		self.placed = False
 
-    def addConnection(self, connection_block_id):
-        self.connections.append(connection_block_id)
+	def addConnection(self, connection_block_id):
+		self.connections.append(connection_block_id)
+
+	def swap_dims(self):
+		x, y = y, x
+
+	def get_id(self):
+		return self.block_id
+
+	def get_width(self):
+		return self.width
+	
+	def get_height(self):
+		return self.height
+	
+	def set_placed(self, placed):
+		self.placed = placed
+	
+	def set_x(self, x):
+		self.x = x
+	
+	def set_y(self, y):
+		self.y = y
+
---
+++ Floorplan.py
@@ -0,0 +1,32 @@
+# Floorplan class
+import copy
+
+class Floorplan:
+	def __init__(self, max_width, max_height):
+		self.cur_width = 0
+		self.cur_height = 0
+		self.max_width = max_width
+		self.max_height = max_height
+		self.grid = [[0 for x in range(max_width)] for y in range(max_height)]
+		self.blocks = []
+	
+	def place_block(self, block):
+		self.blocks.append(copy.deepcopy(block))
+		
+		self.cur_width = max(block.x + block.width, self.cur_width)
+		self.cur_height = max(block.y + block.height, self.cur_height)
+		
+		for x in range(block.x, block.x + block.width):
+			for y in range(block.y, block.y + block.height):
+				assert(self.grid[y][x] != 0), "Place not free to place block!"
+				grid[y][x] = 1
+	
+	def get_grid(self):
+		return self.grid
+
+	def get_cur_dims(self):
+		return self.cur_width, self.cur_height
+
+	def get_max_dims(self):
+		return self.max_width, self.max_height
+	
