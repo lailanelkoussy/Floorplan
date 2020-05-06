6f2adc1eafb5322ce102768b91f1dd5d2afac2f4
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2020-05-07 01:36:58 +0200

    examples 1 and 4 are the ones with width and height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import Floor_planning
 
-with open('example4.json') as json_file:
+with open('example1.json') as json_file:
     data = json.load(json_file)
     my_floorplan = Floor_planning.make_floorplan(data, 0.6)
